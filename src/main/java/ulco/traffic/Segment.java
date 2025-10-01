25ccc1173cb32fd64005d3dcce16b4ae448b2a3b
mathieu-rahab <mathieu@MacBook-Pro-de-Mathieu.local>
2025-10-01 19:53:24 +0200

    étape 7

--- src/main/java/ulco/traffic/Segment.java
+++ src/main/java/ulco/traffic/Segment.java
@@ -1,17 +1,32 @@
 package ulco.traffic;
 
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
 
 public class Segment {
+    public Segment(int length, int speed, TypeEnd typeEnd) {
+        _length = length;
+        _speed = speed;
+        _typeEnd = typeEnd;
+        _lastVehiculeEntryTime = INIT_LAST_VEHICULE_ENTRY_TIME;
+        _waitingTimes = new java.util.HashMap<>();
+    }
+
     public Segment(int length, int speed) {
         _length = length;
         _speed = speed;
+        _typeEnd = TypeEnd.NONE;
+        _lastVehiculeEntryTime = INIT_LAST_VEHICULE_ENTRY_TIME;
+        _waitingTimes = new java.util.HashMap<>();
     }
 
     public void addVehicle(Vehicle vehicle) {
+        System.out.println("Ajout du véhicule " + vehicle.getId() + " au segment (" + this.hashCode() + ")");
         vehicle.setSpeed(_speed);
         _vehicles.add(vehicle);
+        vehicle.setDistance(0.0);
+        _lastVehiculeEntryTime = 0.0;
     }
 
     public Boolean hasVehicule(String id){
@@ -23,16 +38,48 @@ public class Segment {
         return false;
     }
 
-    public void run(Double time) {
+public void run(Double time) {
+    _lastVehiculeEntryTime += time;
+    // Avancer tous les véhicules et actualiser le temps d'attente si besoin
     for (Vehicle v : _vehicles) {
-        v.setDistance(v.getDistance() + (time * _speed));
+        v.setDistance(v.getDistance() + (time * v.getSpeed()));
+        if (vehiculeAtEnd(v.getId())) {
+            v.setSpeed(0);
+            if (_typeEnd == TypeEnd.STOP_SIGN ) {
+                double waitingTime = _waitingTimes.getOrDefault(v.getId(), 0.0) + time;
+                _waitingTimes.put(v.getId(), waitingTime);
+            }
+        }
     }
+    // Traiter les véhicules en tête qui peuvent sortir
     while (!_vehicles.isEmpty() && vehiculeAtEnd(_vehicles.peek().getId()) && _nextSegment != null) {
-        Vehicle v = _vehicles.poll();
-        v.setDistance(0.0);
-        _nextSegment.addVehicle(v);
+        Vehicle v = _vehicles.peek();
+        if (_typeEnd == TypeEnd.STOP_SIGN) {
+            double waitingTime = _waitingTimes.getOrDefault(v.getId(), 0.0);
+            if (waitingTime >= 5.0 && _nextSegment.getLastVehiculeEntryTime() >= 10) {
+                moveVehicleToNextSegment(v.getId());
+                _waitingTimes.remove(v.getId());
+            } else {
+                break; // On ne peut pas sortir, on arrête la boucle
+            }
+        } else {
+            moveVehicleToNextSegment(v.getId());
+        }
     }
 }
+    // méthode appélée lorsque que toutes les condition sont reunies pour faire passer un véhicule au segment suivant
+    private void moveVehicleToNextSegment(String id) {
+        if (hasVehicule(id) && vehiculeAtEnd(id) && _nextSegment != null) {
+            Vehicle v = getVehicle(id);
+            _vehicles.remove(v);
+            _nextSegment.addVehicle(v);
+            if (_waitingTimes.containsKey(v.getId())) {
+                _waitingTimes.remove(v.getId());
+            }
+
+
+        }
+    }
 
     public boolean vehiculeAtEnd(String id){
         return hasVehicule(id) && getVehicle(id).getDistance() >= _length;
@@ -55,9 +102,22 @@ public class Segment {
         return null;
     }
 
+    public double getLastVehiculeEntryTime() {
+        return _lastVehiculeEntryTime;
+    }
+
     private int _length;
     private int _speed;
     private Queue<Vehicle> _vehicles = new LinkedList<>();
     private Segment _nextSegment;
+    private TypeEnd _typeEnd;
+    private double _lastVehiculeEntryTime;
+    private Map<String, Double> _waitingTimes;
+
+    public static enum TypeEnd {
+        STOP_SIGN,
+        NONE
+    }
+    public static int INIT_LAST_VEHICULE_ENTRY_TIME = 100;
 
 }
---
+++ src/test/java/ulco/traffic/Story_7.java
@@ -0,0 +1,51 @@
+package ulco.traffic;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Story_7 {
+    @Test
+    @DisplayName("Segment is created  Vehicule_Segment")
+    public void testSegment() {
+
+        Segment segment1 = new Segment(200, 10, Segment.TypeEnd.STOP_SIGN);
+        Segment segment2 = new Segment(150, 7, Segment.TypeEnd.STOP_SIGN);
+
+        segment1.setNextSegment(segment2);
+
+        Vehicle vehicle1 = new Vehicle();
+        Vehicle vehicle2 = new Vehicle();
+
+
+        segment1.addVehicle(vehicle1);
+        segment1.addVehicle(vehicle2);
+
+        for (int i = 0; i < 20; i += 1) {
+            segment1.run(1.0);
+        }
+
+        assertTrue(segment1.hasVehicule(vehicle1.getId()));
+        assertTrue(segment1.hasVehicule(vehicle2.getId()));
+
+        segment1.run(5.0);
+
+        assertFalse(segment1.hasVehicule(vehicle1.getId()));
+        assertTrue(segment2.hasVehicule(vehicle1.getId()));
+
+        double distance = vehicle2.getDistance();
+        for (int i = 0; i <= 10; i += 1) {
+            assertEquals(distance, vehicle2.getDistance());
+            segment1.run(1.0);
+            segment2.run(1.0);
+        }
+
+        assertFalse(segment1.hasVehicule(vehicle2.getId()));
+        assertTrue(segment2.hasVehicule(vehicle2.getId()));
+
+    }
+
+}
